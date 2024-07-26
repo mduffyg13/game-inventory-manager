69c8c678930a831fcef098b64b92c25e9708223b
mduffyg13@gmail.com <mduffyg13@gmail.com>
2024-07-26 16:13:28 +0100

    add test db json. update mock data source

--- GameClient/src/Game.jsx
+++ GameClient/src/Game.jsx
@@ -3,51 +3,65 @@ import { useState, useEffect } from 'react';
 import GameList from './GameList';
 
 const term = "Game";
+const API_URL = '/games';
 
 function Game() {
   const [data, setData] = useState([]);
-  const [maxId, setMaxId] = useState(0);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetchGameData();
   }, []);
 
   const fetchGameData = () => {
-    // Simulate fetching data from API
-    const gameData = [
-      { id: 1, name: 'ICO', platform: 'PS2' },
-      { id: 2, name: 'Yakuza', platform: 'PS2' },
-      { id: 3, name: 'Super Mario Sunshine', platform: 'NGC' },
-    ];
-    setData(gameData);
-    setMaxId(Math.max(...gameData.map(x => x.id)));
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => setError(error));
   };
 
   const handleCreate = (item) => {
-    // Simulate creating item on API
-    const newItem = { ...item, id: data.length + 1 };
-    setData([...data, newItem]);
-    setMaxId(maxId + 1);
+
+    console.log(`add item: ${JSON.stringify(item)}`)
+
+    fetch(API_URL, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({name: item.name, platform: item.platform}),
+    })
+      .then(response => response.json())
+      .then(returnedItem => setData([...data, returnedItem]))
+      .catch(error => setError(error));
   };
 
-  const handleUpdate = (item) => {
-    // Simulate updating item on API
-    const updatedData = data.map(x => x.id === item.id ? item : x);
-    setData(updatedData);
+  const handleUpdate = (updatedItem) => {
+
+    console.log(`update item: ${JSON.stringify(updatedItem)}`)
+
+    fetch(`${API_URL}/${updatedItem.id}`, {
+      method: 'PUT',
+      headers,
+      body: JSON.stringify(updatedItem),
+    })
+      .then(() => setData(data.map(item => item.id === updatedItem.id ? updatedItem : item)))
+      .catch(error => setError(error));
   };
 
   const handleDelete = (id) => {
-    // Simulate deleting item on API
-    const updatedData = data.filter(x => x.id !== id);
-    setData(updatedData);
+    fetch(`${API_URL}/${id}`, {
+      method: 'DELETE',
+      headers,
+    })
+      .then(() => setData(data.filter(item => item.id !== id)))
+      .catch(error => console.error('Error deleting item:', error));
   };
-
 
   return (
     <div>
       <GameList
         name={term}
         data={data}
+        error={error}
         onCreate={handleCreate}
         onUpdate={handleUpdate}
         onDelete={handleDelete}
